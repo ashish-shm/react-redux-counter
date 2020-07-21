889a4296a18fd8eadfd39f8ea0e8df721a38afd8
Ashish Sharma <ashish.shm09@gmail.com>
2020-07-21 13:14:06 +0530

    Counter Complete

---
+++ src/store/store.js
@@ -0,0 +1,22 @@
+import { createStore } from "redux";
+import { INCREMENT, DECREMENT, RESET } from "./actionTypes";
+
+const initialState = {
+    count : 0,
+    max : 50
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT:
+      return {...state, count : state.count +1};
+    case DECREMENT:
+      return {...state, count : state.count - 1};
+    case RESET:
+      return {...state, count : 0};
+    default:
+      return state;
+  }
+}
+
+export let store = createStore(reducer);
